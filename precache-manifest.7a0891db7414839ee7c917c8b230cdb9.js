45aaf314d3cd49f0f4d510df8acd9f67f3a55c3b
Mark <Trrrrex93@gmail.com>
2020-06-09 12:53:55 +0400

    Updates

---
+++ precache-manifest.7a0891db7414839ee7c917c8b230cdb9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58e9905f41490b08ec4a80c2f661082e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "db597feca601bc0f0ccd",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d4424019a61c769c223c",
+    "url": "/portfolio/static/css/main.0e213957.chunk.css"
+  },
+  {
+    "revision": "db597feca601bc0f0ccd",
+    "url": "/portfolio/static/js/2.a3282f9c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.a3282f9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4424019a61c769c223c",
+    "url": "/portfolio/static/js/main.2a4326a6.chunk.js"
+  },
+  {
+    "revision": "0c18a05f1fb6137df5ad",
+    "url": "/portfolio/static/js/runtime-main.24d195ed.js"
+  },
+  {
+    "revision": "31d41d95049ca8f69b5508026142aa97",
+    "url": "/portfolio/static/media/1.31d41d95.png"
+  },
+  {
+    "revision": "d8dc501ca41d23d86d8b9deda0bc617b",
+    "url": "/portfolio/static/media/2.d8dc501c.png"
+  },
+  {
+    "revision": "99bde39bc06ea48e0dbb1ec26409c24e",
+    "url": "/portfolio/static/media/3.99bde39b.png"
+  },
+  {
+    "revision": "696f29d3acebcce2af0cc3e2f88fa6a1",
+    "url": "/portfolio/static/media/LPLx1R9fl4Q.696f29d3.jpg"
+  },
+  {
+    "revision": "cf3b1ee30e9efeedbcb92ce3da9c2cf2",
+    "url": "/portfolio/static/media/bright-daylight-environment-forest-240040.cf3b1ee3.jpg"
+  },
+  {
+    "revision": "dc442f4fafeca60ee087d088db68cbc7",
+    "url": "/portfolio/static/media/forest-covered-in-white-fog-1423600.dc442f4f.jpg"
+  },
+  {
+    "revision": "1421e57668d726144c92a0d0c35687fe",
+    "url": "/portfolio/static/media/gray-bridge-and-trees-814499.1421e576.jpg"
+  },
+  {
+    "revision": "bbabf1fc92ef1cff55d4dc727297a4fd",
+    "url": "/portfolio/static/media/nature-forest-trees-park-38136.bbabf1fc.jpg"
+  }
+]);
