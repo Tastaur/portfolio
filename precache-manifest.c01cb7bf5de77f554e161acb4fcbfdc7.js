51b142edc861a09746b2190f255fd541c27baef4
Mark <Trrrrex93@gmail.com>
2020-06-22 00:25:34 +0400

    Updates

---
+++ precache-manifest.c01cb7bf5de77f554e161acb4fcbfdc7.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0476349e66d386f4405aa7b4081ee3a6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fb651e49b588fe9673f3",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6f4aaa91bc44d930911b",
+    "url": "/portfolio/static/css/main.50f031dc.chunk.css"
+  },
+  {
+    "revision": "fb651e49b588fe9673f3",
+    "url": "/portfolio/static/js/2.6d09e38b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.6d09e38b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f4aaa91bc44d930911b",
+    "url": "/portfolio/static/js/main.cfa5b912.chunk.js"
+  },
+  {
+    "revision": "e5793b53fd44ff7c926f",
+    "url": "/portfolio/static/js/runtime-main.6a088b17.js"
+  },
+  {
+    "revision": "31d41d95049ca8f69b5508026142aa97",
+    "url": "/portfolio/static/media/1.31d41d95.png"
+  },
+  {
+    "revision": "3c605320b531b4688832908ebf1493be",
+    "url": "/portfolio/static/media/1.3c605320.jpg"
+  },
+  {
+    "revision": "017d843008249f90279b97fda754f5f5",
+    "url": "/portfolio/static/media/2.017d8430.jpg"
+  },
+  {
+    "revision": "d8dc501ca41d23d86d8b9deda0bc617b",
+    "url": "/portfolio/static/media/2.d8dc501c.png"
+  },
+  {
+    "revision": "99bde39bc06ea48e0dbb1ec26409c24e",
+    "url": "/portfolio/static/media/3.99bde39b.png"
+  },
+  {
+    "revision": "9fcf169b72bae966b3c46d1526c103ee",
+    "url": "/portfolio/static/media/3.9fcf169b.jpg"
+  },
+  {
+    "revision": "3aa32c9bf19aaedfbead1cf8857b4420",
+    "url": "/portfolio/static/media/4.3aa32c9b.png"
+  },
+  {
+    "revision": "88e1a76b143b01368d3d0ab5816fd471",
+    "url": "/portfolio/static/media/4.88e1a76b.jpg"
+  },
+  {
+    "revision": "696f29d3acebcce2af0cc3e2f88fa6a1",
+    "url": "/portfolio/static/media/LPLx1R9fl4Q.696f29d3.jpg"
+  },
+  {
+    "revision": "5b440c14d989000af2276308359709c1",
+    "url": "/portfolio/static/media/call.5b440c14.png"
+  },
+  {
+    "revision": "aef9c7e74703ed2cf3e5b29ce83a33bc",
+    "url": "/portfolio/static/media/facebook.aef9c7e7.png"
+  },
+  {
+    "revision": "1b9b9f71269e504156ce9d89a00f2551",
+    "url": "/portfolio/static/media/github.1b9b9f71.png"
+  },
+  {
+    "revision": "817d585a7dd0775dbc9b41927bd043ca",
+    "url": "/portfolio/static/media/telegram.817d585a.png"
+  },
+  {
+    "revision": "f4ab5cc172d0912ac76f48d55e67a035",
+    "url": "/portfolio/static/media/twitter.f4ab5cc1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.9f301ed5c900d6ffd4093e974cfeb4db.js"
+  "/portfolio/precache-manifest.c01cb7bf5de77f554e161acb4fcbfdc7.js"
 );
 
 self.addEventListener('message', (event) => {
