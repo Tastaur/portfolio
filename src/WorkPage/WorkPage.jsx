874217fc7c6a51cd7aa39f91fd86bcb517584da8
Mark <Trrrrex93@gmail.com>
2020-06-22 00:34:07 +0400

    add new slide

--- src/WorkPage/WorkPage.jsx
+++ src/WorkPage/WorkPage.jsx
@@ -5,10 +5,14 @@ import firstSlide from './1.png'
 import secondSlide from './2.png'
 import thirdSlide from './3.png'
 import fourthSlide from './4.png'
+import fivthSlide from './5.png'
+import sixSlide from './6.png'
 import firstSlideMain from '../photo/1.jpg'
 import secondSlideMain from '../photo/2.jpg'
 import thirdSlideMain from '../photo/3.jpg'
 import fourthSlideMain from '../photo/4.jpg'
+import fivthSlideMain from '../photo/5.jpg'
+import sixSlideMain from '../photo/6.jpg'
 import s from './WorkPage.module.css'
 
 const WorkPage = (props) => {
@@ -58,6 +62,8 @@ const WorkPage = (props) => {
           <p>Использовались: HTML, CSS, JavaScript</p>
         </Carousel.Caption>
       </Carousel.Item>
+
+
       <Carousel.Item>
         <img
             className={s.item}
@@ -73,6 +79,43 @@ const WorkPage = (props) => {
           /></a>
           <h3>Небольшое тестовое задание</h3>
           <p>Использовались: React, Redux, React-Bootstrap</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img
+            className={s.item}
+            src={fivthSlideMain}
+            alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <a href='https://transpot-group.firebaseapp.com/auth/'><img
+              src={fivthSlide}
+              className={s.workImg}
+              alt="Third slide"
+          /></a>
+          <h3>Приложение для аренды спец техники (вёрстка)</h3>
+          <p>Использовались: Ionic</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+
+      <Carousel.Item>
+        <img
+            className={s.item}
+            src={sixSlideMain}
+            alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <a href='https://tastaur.github.io/test-form/'><img
+              src={sixSlide}
+              className={s.workImg}
+              alt="Third slide"
+          /></a>
+          <h3>Работа с формами</h3>
+          <p>Использовались: React, React-Redux, Redux-Form</p>
         </Carousel.Caption>
       </Carousel.Item>
 
